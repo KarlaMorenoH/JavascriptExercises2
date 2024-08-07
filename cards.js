66471cee8a9e43622b841d8967ff55e348c0321a
KarlaMorenoH <karla.moreno3120@gmail.com>
2024-08-07 16:21:20 -0600

    agrego código con verificaciones

--- cards.js
+++ cards.js
@@ -3,33 +3,35 @@ const IMAGE_URL = "https://placehold.co/200";
 const users = [
     {
         id: 1,
-        user_name: 'User1',
+        user_name: 'Eduardo',
         description: 'lorem ipsum',
         age: '46',
         fav_music: {
             bands: [
-                'Band 1', 'Band 2', 'Band 3', 'Band 4'
+                'Korn', 'Slipknot', 'Deftones', 'Papa Roach'
             ]
         }
     },
     {
         id: 2,
-        user_name: 'User LastName',
+        user_name: 'Karla',
         description: 'Lorem lorem lorem',
         age: '23',
         fav_music: {
             bands: [
-                'Band 1', 'Band 2', 'Band 3', 'Band 4'
+                'Dua Lipa', 'Lady Gaga', 'Billie Eillish', 'Lana del Rey'
             ]
         }
     }
 ];
 
 const cardContainer = document.getElementById("card-container");
+console.log('Card container:', cardContainer); //Verifico mi elemento creado
 
 function createUserCard(user) {
-    const card = document.createElement("div");
-    card.classList.add("card");
+    /*const card = document.createElement("div");
+    card.classList.add("card");*/
+    console.log('Creating card for user:', user); // Volvemos a verificar
 
     const cardImgContainer = document.createElement("div");
     cardImgContainer.classList.add("card-image-container");
@@ -52,8 +54,11 @@ function createUserCard(user) {
     const cardFavMusicElement = document.createElement("p");
     cardFavMusicElement.textContent = `Favorite Bands: ${user.fav_music.bands.join(', ')}`;
 
-    card.append(cardImgContainer, cardTitleElement, cardAgeElement, cardDescElement, cardFavMusicElement);
-    cardContainer.appendChild(card);
+    /*card.append(cardImgContainer, cardTitleElement, cardAgeElement, cardDescElement, cardFavMusicElement);
+    cardContainer.appendChild(card);*/
 }
 
+
+//Hasta aqui obtengo "Creating card for user en la consola de la página"
+// puedo desbloquear el contenido activando el código de card.append
 users.forEach(createUserCard);
